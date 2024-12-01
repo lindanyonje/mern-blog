22ed197d95b54de91138e323c7a2303561c5052f
Linda <lindaatieno@gmail.com>
2024-12-01 13:55:03 -0500

    render

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const postRoutes = require('./routes/postRoutes')
 const {notFound, errorHandler} = require('./middleware/errorMiddleware')
 
 
-
 const app = express();
 app.use(express.json({extended:true}))
 app.use(express.urlencoded({extended: true}))
